f10f2708a8d3b3220505903207c54290bf6c7688
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2023-05-27 15:30:28 +0530

    error ixed again

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={inter.className}>
         <Providers>
           <Header />
